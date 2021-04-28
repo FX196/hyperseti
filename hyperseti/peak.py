Fix NUM_BLOCKS calculation

hyperseti/peak.py
@@ -101,8 +101,10 @@ def prominent_peaks_optimized(img, min_xdistance=1, min_ydistance=1, threshold=N
     Modified from https://github.com/mritools/cupyimg _prominent_peaks method
     """
     THREADS_PER_BLOCK = (2, 2)
-    NUM_BLOCKS =  (img.shape[1] // (THREADS_PER_BLOCK[1] * min_xdistance) + ((img.shape[1] % (THREADS_PER_BLOCK[1] * min_xdistance)) > 0),
-                   img.shape[0] // (THREADS_PER_BLOCK[0] * min_ydistance) + ((img.shape[0] % (THREADS_PER_BLOCK[0] * min_ydistance)) > 0))
+    # Each thread is responsible for a (min_ydistance * min_xdistance) patch
+    # THREADS_PER_BLOCK is in the order of (x, y), but img.shape is in the order of (y, x)
+    NUM_BLOCKS =  (img.shape[1] // (THREADS_PER_BLOCK[0] * min_xdistance) + ((img.shape[1] % (THREADS_PER_BLOCK[0] * min_xdistance)) > 0),
+                   img.shape[0] // (THREADS_PER_BLOCK[1] * min_ydistance) + ((img.shape[0] % (THREADS_PER_BLOCK[1] * min_ydistance)) > 0))
     NUM_THREADS = np.multiply(THREADS_PER_BLOCK, NUM_BLOCKS)
     elems = (NUM_THREADS[0] * NUM_THREADS[1],)
     intensity, xcoords, ycoords = cp.zeros(elems, dtype=cp.float32), cp.zeros(elems, dtype=cp.int32), cp.zeros(elems, dtype=cp.int32)