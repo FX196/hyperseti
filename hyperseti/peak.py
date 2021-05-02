5897b712fab392ec9d9440c4245cacf1895b52c8
Jie Qiu <jieqiu0630@gmail.com>
2021-05-02 00:15:47 -0700

    This is too complicated

--- hyperseti/peak.py
+++ hyperseti/peak.py
@@ -8,8 +8,8 @@ from .log import logger_group, Logger
 logger = Logger('hyperseti.peak')
 logger_group.add_logger(logger)
 
-prominent_peaks_kernel = cp.RawKernel(r'''
- extern "C" __global__
+construct_max_buffer = cp.RawKernel(r'''
+extern "C" __global__
     __global__ void prominent_peaks_kernel
         (const float *img, int M, int N, int min_xdistance, int min_ydistance, float threshold, float *intensity, int *xcoords, int *ycoords, float *max_intensity)
         /* Each thread computes a different dedoppler sum for a given channel
@@ -27,7 +27,6 @@ prominent_peaks_kernel = cp.RawKernel(r'''
          * ycoords: returned y coordinates of peak intensities.
          */
         {
-            // printf("block dim x: %d, block dim y: %d\n", blockDim.x, blockDim.y);
             // Setup thread index
             const int tx = blockIdx.x * blockDim.x + threadIdx.x;
             const int ty = blockIdx.y * blockDim.y + threadIdx.y;
@@ -36,7 +35,6 @@ prominent_peaks_kernel = cp.RawKernel(r'''
             const int p_end_x = min(N, p_start_x + min_xdistance);
             const int p_end_y = min(M, p_start_y + min_ydistance);
             const int p_mid_y = (p_start_y + p_end_y) / 2;
-
             // Find local maximum pixel
             float intensity_max = -1.0;
             int x_max = -1;
@@ -57,7 +55,7 @@ prominent_peaks_kernel = cp.RawKernel(r'''
                             max_intensity[idx] = img[idx];
                         } else {
                             assert(idx - N >= 0);
-                            max_intensity[idx] = max(img[idx], max_intensity[idx - N]);
+                            max_intensity[idx] = max_intensity[idx - N] > img[idx] ? max_intensity[idx - N] : img[idx];
                         }
                     }
                 }  
@@ -74,10 +72,110 @@ prominent_peaks_kernel = cp.RawKernel(r'''
                             max_intensity[idx] = img[idx];
                         } else {
                             assert(idx + N < N * M);
-                            max_intensity[idx] = max(img[idx], max_intensity[idx + N]);
+                            max_intensity[idx] = max_intensity[idx + N] > img[idx] ? max_intensity[idx + N] : img[idx];
+                            if (y == 202 && x == 1) {
+                                printf("hihi %f %f x: %d\n", max_intensity[idx], img[idx], x);
+                            }
                         }
                     }
                 } 
+                if (p_end_y == 208 && p_start_x == 0) {
+                    int y = 202;
+                    int x = 1;
+                    printf("OH NO %f %f %d %d\n", max_intensity[(y-1) * N + x], max_intensity[182 * N + x], p_start_y, p_mid_y);
+                }
+            } else { // Block cannot be divided in half
+                for (int y = p_start_y; y < p_end_y; ++y) {
+                    for (int x = p_start_x; x < p_end_x; ++x) {
+                        // Apply threshold
+                        int idx = y * N + x;
+                        if (img[idx] > intensity_max) {
+                            intensity_max = img[idx];
+                            x_max = x;
+                            y_max = y;
+                        }
+                    }
+                } 
+            }
+        }
+''', 'max_buffer_kernel')
+
+prominent_peaks_kernel = cp.RawKernel(r'''
+ extern "C" __global__
+    __global__ void prominent_peaks_kernel
+        (const float *img, int M, int N, int min_xdistance, int min_ydistance, float threshold, float *intensity, int *xcoords, int *ycoords, float *max_intensity)
+        /* Each thread computes a different dedoppler sum for a given channel
+
+         * img: image, (M x N) shape
+         * M: height of img
+         * N: width of img
+         * min_xdistance: Minimum distance separating features in the x dimension.
+         * min_ydistance: Minimum distance separating features in the y dimension.
+         * threshold: Minimum intensity of peaks. Default is `0.5 * max(image)`.
+         * num_peaks: Maximum number of peaks. When the number of peaks exceeds `num_peaks`,
+         *            return `num_peaks` coordinates based on peak intensity.
+         * intensity: array of floats that hold the returned peak intensities.
+         * xcoords: returned x coordinates of peak intensities.
+         * ycoords: returned y coordinates of peak intensities.
+         */
+        {
+            // Setup thread index
+            const int tx = blockIdx.x * blockDim.x + threadIdx.x;
+            const int ty = blockIdx.y * blockDim.y + threadIdx.y;
+            const int p_start_x = tx * min_xdistance;
+            const int p_start_y = ty * min_ydistance;
+            const int p_end_x = min(N, p_start_x + min_xdistance);
+            const int p_end_y = min(M, p_start_y + min_ydistance);
+            const int p_mid_y = (p_start_y + p_end_y) / 2;
+            // Find local maximum pixel
+            float intensity_max = -1.0;
+            int x_max = -1;
+            int y_max = -1;
+            // If block can be divided in half
+            int can_divide = p_mid_y > p_start_y && p_mid_y < p_end_y - 1;
+            if (can_divide) {
+                for (int y = p_start_y; y < p_mid_y; ++y) {
+                    for (int x = p_start_x; x < p_end_x; ++x) {
+                        // Apply threshold
+                        int idx = y * N + x;
+                        if (img[idx] > intensity_max) {
+                            intensity_max = img[idx];
+                            x_max = x;
+                            y_max = y;
+                        }
+                        if (y == p_start_y) {
+                            max_intensity[idx] = img[idx];
+                        } else {
+                            assert(idx - N >= 0);
+                            max_intensity[idx] = max_intensity[idx - N] > img[idx] ? max_intensity[idx - N] : img[idx];
+                        }
+                    }
+                }  
+                for (int y = p_end_y - 1; y >= p_mid_y; --y) {
+                    for (int x = p_start_x; x < p_end_x; ++x) {
+                        // Apply threshold
+                        int idx = y * N + x;
+                        if (img[idx] > intensity_max) {
+                            intensity_max = img[idx];
+                            x_max = x;
+                            y_max = y;
+                        }
+                        if (y == p_end_y - 1) {
+                            max_intensity[idx] = img[idx];
+                        } else {
+                            assert(idx + N < N * M);
+                            max_intensity[idx] = max_intensity[idx + N] > img[idx] ? max_intensity[idx + N] : img[idx];
+                            if (y == 202 && x == 1) {
+                                printf("hihi %f %f x: %d\n", max_intensity[idx], img[idx], x);
+                            }
+                        }
+                    }
+                } 
+                if (p_end_y == 208 && p_start_x == 0) {
+                    int y = 202;
+                    int x = 1;
+                    printf("OH NO %f %f %d %d\n", max_intensity[(y-1) * N + x], max_intensity[182 * N + x], p_start_y, p_mid_y);
+                }
             } else { // Block cannot be divided in half
                 for (int y = p_start_y; y < p_end_y; ++y) {
                     for (int x = p_start_x; x < p_end_x; ++x) {
@@ -136,7 +234,7 @@ prominent_peaks_kernel = cp.RawKernel(r'''
                             }
                         }
                     } else {
-                        for (int y = peak_check_min_x; y < peak_check_max_y; ++y) {
+                        for (int y = p_end_x; y < peak_check_max_y; ++y) {
                             for (int x = peak_check_min_x; x < peak_check_max_x; ++x) {
                                 if (img[y * N + x] > intensity_max) {
                                     is_peak = 0;
@@ -155,9 +253,15 @@ prominent_peaks_kernel = cp.RawKernel(r'''
                     assert(start_y_above < mid_y_above && mid_y_above < end_y_above);
                     assert(end_y_above - min_ydistance == start_y_above);
                     if (peak_check_min_y < mid_y_above) {
+                        if (y_max == 208 && x_max == 8) {
+                            printf("intensity max %f\n", intensity_max);
+                        }
                         // Check against the aggregated max
                         for (int x = peak_check_min_x; x < peak_check_max_x; ++x) {
                             int idx = mid_y_above * N + x;
+                            if (x == 1 && p_start_y == 208) {
+                                printf("what %f %f %d\n", max_intensity[182 * N + 1], img[182 * N + 1], mid_y_above);
+                            }
                             if (max_intensity[idx] > intensity_max) {
                                 is_peak = 0;
                                 break;
@@ -187,7 +291,7 @@ prominent_peaks_kernel = cp.RawKernel(r'''
                 // Check elements in the left block
                 if (is_peak && tx > 0 && can_divide) {
                     for (int x = peak_check_min_x; x < p_start_x; x++) {
-                        float top_max = max_intensity[(p_mid_y - 1) * N + x]; // Aggregated sum from top
+                        float top_max = max_intensity[(p_mid_y - 1) * N + x]; // Aggregated max from top
                         float bottom_max = max_intensity[p_mid_y * N + x];
                         if (top_max > intensity_max || bottom_max > intensity_max) {
                             is_peak = 0;
@@ -208,7 +312,7 @@ prominent_peaks_kernel = cp.RawKernel(r'''
                 // Check elements in the right block
                 if (is_peak && tx < blockDim.x * gridDim.x - 1 && can_divide) {
                     for (int x = p_end_x; x < peak_check_max_x; x++) {
-                        float top_max = max_intensity[(p_mid_y - 1) * N + x]; // Aggregated sum from top
+                        float top_max = max_intensity[(p_mid_y - 1) * N + x]; // Aggregated max from top
                         float bottom_max = max_intensity[p_mid_y * N + x];
                         if (top_max > intensity_max || bottom_max > intensity_max) {
                             is_peak = 0;
@@ -276,6 +380,9 @@ def prominent_peaks_optimized(img, min_xdistance=1, min_ydistance=1, threshold=N
     intensity, xcoords, ycoords, max_intensity = cp.zeros(elems, dtype=cp.float32), cp.zeros(elems, dtype=cp.int32), cp.zeros(elems, dtype=cp.int32), cp.zeros(elems, dtype=cp.float32)
     prominent_peaks_kernel(NUM_BLOCKS, THREADS_PER_BLOCK, (img, cp.int32(img.shape[0]), cp.int32(img.shape[1]), cp.int32(min_xdistance), cp.int32(min_ydistance), cp.float32(threshold), intensity, xcoords, ycoords, max_intensity))
     cp.cuda.Stream.null.synchronize()
+    # for i in range(len(intensity)):
+    #     if intensity[i] != 0:
+    #         print(xcoords[i], ycoords[i], intensity[i], min_xdistance, min_ydistance)
     indices = intensity != 0.0
     return intensity[indices], xcoords[indices], ycoords[indices]
 
