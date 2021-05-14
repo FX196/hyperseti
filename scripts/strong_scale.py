9034bbce4b0f527ceb3c3da0349350322a91f93a
Yuhong Chen <ychen@blpc0.bl.pvt>
2021-05-13 17:08:09 -0700

    update parameters

--- scripts/strong_scale.py
+++ scripts/strong_scale.py
@@ -4,7 +4,7 @@ import sys
 for i in [1, 2, 3, 4, 8, 16, 32, 40]:
     sys.stderr.write(str(i) + "\n")
     print(f"###### {i} ######")
-    hyperseti.find_et_parallel("GBT_58210_31246_HIP93185_fine.h5", nranks=i, ngpu=4, gulp_size=2**12, ngulps=440+i, freq_start=116048342, apply_normalization=True)
+    hyperseti.find_et_parallel("GBT_58210_31246_HIP93185_fine.h5", nranks=i, ngpu=4, max_dd=5, threshold=25, gulp_size=2**18, ngulps=160+i, freq_start=116048342, apply_normalization=True)
 
 
 # for i in [1, 2, 3, 4]:
--- scripts/weak_scale.py
+++ scripts/weak_scale.py
@@ -6,4 +6,4 @@ for i in [1, 2, 3, 4]:
     nranks, ngpu = i * 4, i
     sys.stderr.write(str(i) + "\n")
     print(f"###### {i} ######")
-    hyperseti.find_et_parallel("GBT_58210_31246_HIP93185_fine.h5", nranks=nranks, ngpu=ngpu, gulp_size=2**12, ngulps=120 * i + nranks, freq_start=116048342, apply_normalization=True)
+    hyperseti.find_et_parallel("GBT_58210_31246_HIP93185_fine.h5", nranks=nranks, ngpu=ngpu, max_dd=5, threshold=25, gulp_size=2**18, ngulps=64 * i + nranks, freq_start=116048342, apply_normalization=True)
